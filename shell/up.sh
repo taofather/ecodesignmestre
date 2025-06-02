207da07fea195c9e140c2181d11753e604c7022a
Jordi Caballero <jordi.caballero@consentio.co>
2025-06-02 16:23:29 +0200

    REQ: attach a couple vids

--- shell/up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-JEKYLL_VERSION=4.0
-
-docker run \
-  --volume="$PWD:/srv/jekyll" \
-  --publish=4000:4000 \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
-  sh -c "chown -R jekyll /usr/gem/ && jekyll serve --trace"
